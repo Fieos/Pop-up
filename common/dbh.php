551dac80a87867667c8d1b69b18d9cca4cac37e2
Daniel Dunmore <danieldunmore333@hotmail.co.uk>
2019-11-04 15:01:22 +0000

    Delete dbh.php

--- common/dbh.php
+++
@@ -1,9 +0,0 @@
-<?php
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "pop-upsports";
-$conn = mysqli_connect($serverName, $dbUsername, $dbPassword,$dbName);
-if (!$conn){
-	die("connection failed: ".mysqli_connect_error());
-}
